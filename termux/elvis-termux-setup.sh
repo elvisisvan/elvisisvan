f703cb14264ec19ec127b044cc9e84e50c8bebb3
Elvis Van <elvisisvan@gmail.com>
2023-12-20 21:44:06 +0700

    add termux setup script

---
+++ termux/elvis-termux-setup.sh
@@ -0,0 +1,12 @@
+termux-setup-storage
+ln -siv /sdcard/src-elvis/elvis-notes/termux/bashrc.md ~/.bashrc
+ln -siv /sdcard/src-elvis/elvis-notes/termux/bash_profile.md ~/.bash_profile
+ln -siv /sdcard/src-elvis/elvis-notes/termux/gitconfig.md ~/.gitconfig
+ln -siv /sdcard/src-elvis/elvis-notes/termux/dottermux ~/.termux
+
+termux-reload-settings
+exec $SHELL -l
+
+pkg update -y
+pkg upgrade -y
+pkg in tsu android-tools nala gh git lazygit
