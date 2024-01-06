bcb7cf05e2a3668a9564541514bf395cbd88d8cb
Elvis Van <elvisisvan@gmail.com>
2024-01-06 16:45:06 +0700

    update termux setup script

--- termux/elvis-termux-setup.sh
+++ termux/elvis-termux-setup.sh
@@ -2,11 +2,11 @@ termux-setup-storage
 ln -siv /sdcard/src-elvis/elvis-notes/termux/bashrc.md ~/.bashrc
 ln -siv /sdcard/src-elvis/elvis-notes/termux/bash_profile.md ~/.bash_profile
 ln -siv /sdcard/src-elvis/elvis-notes/termux/gitconfig.md ~/.gitconfig
-ln -siv /sdcard/src-elvis/elvis-notes/termux/dottermux ~/.termux
+ln -sfv /sdcard/src-elvis/elvis-notes/termux/dottermux/termux.properties ~/.termux
 
 termux-reload-settings
 exec $SHELL -l
 
 pkg update -y
 pkg upgrade -y
-pkg in tsu android-tools nala lsd gh git lazygit
+pkg in x11-repo android-tools tsu nala lsd gh git lazygit broot proot proot-distro cpufetch fastfetch debianutils speedtest-go
